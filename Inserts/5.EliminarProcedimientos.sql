c1421390ac4e5754c168a4c52f66974cd4b85f5a
AlvaroBlas <96362231+AlvaroBlas@users.noreply.github.com>
2025-11-20 12:58:22 -0500

    Add files via upload

---
+++ Inserts/5.EliminarProcedimientos.sql
@@ -0,0 +1,20 @@
+﻿--CREATE PROCEDURE EliminarProcedimientos
+--AS
+--      DECLARE @procName VARCHAR(500)
+--      DECLARE cur CURSOR
+--            FOR SELECT [name] FROM sys.objects WHERE TYPE = 'p'
+--      OPEN cur
+ 
+--      FETCH NEXT FROM cur INTO @procName
+--      WHILE @@fetch_status = 0
+--      BEGIN
+--            IF @procName <> 'EliminarProcedimientos'
+--                  EXEC('drop procedure ' + @procName)
+--                  FETCH NEXT FROM cur INTO @procName
+--      END
+--      CLOSE cur
+--      DEALLOCATE cur
+--GO
+--      GRANT EXECUTE ON dbo.EliminarProcedimientos TO PUBLIC
+--GO
+ 
